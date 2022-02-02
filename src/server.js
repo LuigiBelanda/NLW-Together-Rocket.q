53495e703d28b20306fdffe165b7477cde2d5f5d
LuigiBelanda <luigi.belanda.milani@gmail.com>
2022-02-02 10:25:12 -0300

    Config da pasta public e ajustes para funcionar o css e images

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const server = express();
 const route = require("./routes");
 const path = require("path");
+
+const server = express();
 
 // configs do EJS e pasta views
 server.set("view engine", "ejs");
 server.set("views", path.join(__dirname, "views"));
+
+// Config da pasta public (css e images)
+server.use(express.static("public"));
 
 server.use(route);
 
