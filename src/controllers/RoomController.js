74dc580909559bdd5b4dc750b1a75d540fdf67ca
LuigiBelanda <luigi.belanda.milani@gmail.com>
2022-02-02 20:49:02 -0300

    Colocando regras para nao criarmos o mesmo roomId que ja existe no bd

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -3,24 +3,39 @@ const Database = require("../db/config");
 module.exports = {
   async create(req, res) {
     const db = await Database();
-
     const pass = req.body.password;
 
     let roomId;
+    let isRoom = false;
 
-    for (var i = 0; i < 6; i++) {
-      i == 0
-        ? (roomId = Math.floor(Math.random() * 10).toString())
-        : (roomId += Math.floor(Math.random() * 10).toString());
+    while (isRoom) {
+      // gera o número da sala
+      for (var i = 0; i < 6; i++) {
+        i == 0
+          ? (roomId = Math.floor(Math.random() * 10).toString())
+          : (roomId += Math.floor(Math.random() * 10).toString());
+      }
+
+      // verifica se esse número de sala já existe
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`);
+
+      // para no primeiro id que já existir e retorna
+      isRoom = roomsExistIds.some((roomExistId) => {
+        roomExistId === roomId;
+      });
     }
 
-    await db.run(`INSERT INTO rooms (
+    // se não achar nenhuma sala que bate com o roomId que geramos ele inseri esse id na tabela
+    if (!isRoom) {
+      // Inseri a sala no banco
+      await db.run(`INSERT INTO rooms (
       id, 
       pass
-    ) vALUES (
-      ${parseInt(roomId)},
-      ${pass}
-    )`);
+      ) vALUES (
+        ${parseInt(roomId)},
+        ${pass}
+      )`);
+    }
 
     await db.close();
 
